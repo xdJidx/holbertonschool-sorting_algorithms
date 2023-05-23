ed8777b0ab3fd490b8d0e107ecbc23f59b41a99b
lenne sebastien <sebastienlenne@gmail.com>
2023-05-23 13:22:08 +0200

    update with prototype task 1,2,3

--- sort.h
+++ sort.h
@@ -24,6 +24,8 @@ void bubble_sort(int *array, size_t size);
 void insertion_sort_list(listint_t **list);
 void selection_sort(int *array, size_t size);
 void quick_sort(int *array, size_t size);
-void swap(int *a, int *b);
+void doubleSwap(listint_t **node1, listint_t **node2);
+void quickSortRecursive(int arr[], int low, int high);
+int partition(int arr[], int low, int high);
 
 #endif
